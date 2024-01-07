ddf77153eb150f3544246f8916dc5247b39fb96a
Philipp Rieser <philipp.rieser@gmail.com>
2024-01-07 22:05:20 +0100

    colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,14 +1,9 @@
 return {
     -- nord
     "shaunsingh/nord.nvim",
-    -- "andersevenrud/nordic.nvim",
-    -- "nordtheme/vim",
-    -- "RRethy/nvim-base16",
-    -- "gbprod/nord.nvim",
     lazy = false,
     priority = 1000,
     config = function()
-        -- vim.cmd([[colorscheme base16-nord]])
         vim.cmd([[colorscheme nord]])
     end,
 }
