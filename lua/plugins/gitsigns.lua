b24a9c8e1affc9f2b75d86fecf04c2725ddb56b1
Philipp Rieser <philipp.rieser@gmail.com>
2024-02-06 22:14:56 +0100

    gitsigns

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,29 +1,27 @@
 return {
     "lewis6991/gitsigns.nvim",
     opts = {
-             -- signs = {
-             --     add = { text = "+" },
-             --     change = { text = "~" },
-             --     delete = { text = "_" },
-             --     topdelete = { text = "‾" },
-             --     changedelete = { text = "~" }
-             -- },
-        -- on_attach = function(bufnr)
-            -- local gs = package.loaded.gitsigns
-            -- local function map(mode, l, r, opts)
-            --     opts = opts or {}
-            --     opts.buffer = bufnr
-            --     vim.keymap.set(mode, l, r, opts)
-            -- end
-            --
+        --     change = { text = "~" },
+        --     delete = { text = "_" },
+        --     topdelete = { text = "‾" },
+        --     changedelete = { text = "~" }
+        -- },
+        on_attach = function(bufnr)
+            local gs = package.loaded.gitsigns
+            local function map(mode, l, r, opts)
+                opts = opts or {}
+                opts.buffer = bufnr
+                vim.keymap.set(mode, l, r, opts)
+            end
+
             -- map("n", "<leader>hD", function()
             --     map("n", "<leader>hd", gs.diffthis, { desc = "git diff against index" })
             --     gs.diffthis "~"
             -- end, { desc = "git diff against last commit" })
-            --
-            --
-            -- map("n", "<leader>tb", gs.toggle_current_line_blame, { desc = "toggle git blame line" })
-            -- map("n", "<leader>td", gs.toggle_deleted, { desc = "toggle git show deleted" })
-        -- end
+
+
+            map("n", "<leader>tb", gs.toggle_current_line_blame, { desc = "toggle git blame line" })
+            map("n", "<leader>td", gs.toggle_deleted, { desc = "toggle git show deleted" })
+        end
     },
 }
