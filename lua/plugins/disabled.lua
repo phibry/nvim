cf50de38d1a2ab6c68ad2deb2c041dd6c8d3a7f7
Philipp Rieser (C414972) <philipp.rieser@axa.ch>
2024-12-17 07:39:03 +0100

    remove telescope, introduce fzf

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -2,6 +2,7 @@ return {
   { "akinsho/bufferline.nvim", enabled = false },
   { "folke/noice.nvim", enabled = false },
   { "folke/flash.nvim", enabled = false },
+  { "echasnovski/mini.indentscope", enabled = false },
   { "lukas-reineke/indent-blankline.nvim", enabled = false },
   { "rcarriga/nvim-notify", enabled = false },
   { "nvimdev/dashboard-nvim", enabled = false },
